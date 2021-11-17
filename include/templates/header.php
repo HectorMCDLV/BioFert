2a80c4412fdb1806bfb501a4ad0048771f9e60ab
HectorMCDLV <hectormcdlv@gmail.com>
2021-11-17 13:54:43 -0600

    uwu

---
+++ include/templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biofert</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+   
+    <header class="header">
+        
+        <a hred="index.html"> <img class = "header__logo" src="img/logo.jfif" alt="Logotipo"> </a>
+        
+        <nav class="navegacion">
+            
+            <a class="navegacion__enlace <?php echo $activo===1 ? 'navegacion__enlace--activo' : ''; ?>" href="index.php">Inicio</a>
+            <a class="navegacion__enlace <?php echo $activo===2 ? 'navegacion__enlace--activo' : ''; ?>" href="productos.php">Productos</a>
+            <a class="navegacion__enlace <?php echo $activo===3 ? 'navegacion__enlace--activo' : ''; ?>" href="nosotros.php">Nosotros</a>
+            <a class="navegacion__enlace <?php echo $activo===4 ? 'navegacion__enlace--activo' : ''; ?>" href="contacto.php">Contacto</a>
+            
+        </nav>     
+    </header>
