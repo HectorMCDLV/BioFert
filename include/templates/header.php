8a244a1e764138b5952cb6dbe9d62e7d5666fcda
HectorMCDLV <hectormcdlv@gmail.com>
2021-11-20 16:58:04 -0600

    css???

--- include/templates/header.php
+++ include/templates/header.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biofert</title>
     <link rel="stylesheet" href="//localhost/biofert/css/normalize.css">
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="//localhost/biofert/css/estilo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
