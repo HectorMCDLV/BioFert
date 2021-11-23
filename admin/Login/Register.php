fad14ac0f2b63e950af936e815b71b294cbdc36a
“David <dpruneda0417@live.com>
2021-11-23 12:55:24 -0600

    1231231

--- admin/Login/Register.php
+++ admin/Login/Register.php
@@ -1,62 +1,79 @@
-<?php
+<?php   
+require '../../include/config/connect.php';
 
-    include '../../admin/Login/Session.php';
-    include '../../include/config/connect.php';
 
-    $db = conectarDB();
+    $link = conectarDB();
+    session_start(); 
+    
+    $errores = [];
+    $email = '';
+    $password = '';
+    $nombre = '';
+    $apellido = '';
+    $telefono = '';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-    $nombres = trim($_POST['nombres']);
-    $apellidos = trim($_POST['apellidos']);
-    $email = trim($_POST['email']);
-    $telefono = trim($_POST['telefono']);
-    $password = trim($_POST['contraseña']);
-    $confirm_password = trim($_POST["confirma_contraseña"]);
-    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
-    
-    
-    if($query = $db->prepare("SELECT * FROM cliente WHERE email = ?")) {
-            $error = '';
-            // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-    $query->bind_param('s', $email);
-    $query->execute();
-    // Store the result so we can check if the account exists in the database.
-    $query->store_result();
-            if ($query->num_rows > 0) {
-                 $error .= '<p class="error">The email address is already registered!</p>';
-            } else {
-                // Validate password
-                if (strlen($password ) < 6) {
-                    $error .= '<p class="error">Password must have atleast 6 characters.</p>';
-                }
-                // Validate confirm password
-                if (empty($confirm_password)) {
-                    $error .= '<p class="error">Please enter confirm password.</p>';
-                }   else {
-                    if (empty($error) && ($password != $confirm_password)) {
-                        $error .= '<p class="error">Password did not match.</p>';
-                }
-            }
-            if (empty($error) ) {
-                $insertQuery = " INSERT INTO cliente (nombres, apellidos,telefono,email,contraseña) VALUES ('${nombres}', '${apellidos}','${telefono}'. '${email}', '${passwordHash}')";
-                $resultado = mysqli_query($db, $insertQuery);
-    
-            }
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        $email = mysqli_real_escape_string( $link, filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL ) ) ;
+        $password = mysqli_real_escape_string( $link, $_POST['contraseña'] );
+        $nombre = mysqli_real_escape_string( $link, $_POST['nombres']);
+        $apellido = mysqli_real_escape_string( $link, $_POST['apellidos'] );
+        $telefono = mysqli_real_escape_string($link, $_POST['telefono'] );
+        if(!$email){
+            $errores[] = " El email es obligatorio o no es válido ";
         }
+
+        if(!$password){
+            $errores[] = " El password es obligatorio ";
+        }
+        if(!$nombre){
+            $errores[] = "Un nombre es obligatorio ";
+        }
+
+        if(!$nombre){
+            $errores[] = "Un Apellido es obligatorio ";
+        }
+
+        if(!$telefono){
+            $errores[] = "El telefono es obligatorio ";
+        }
+
+        if(!$apellido){
+            $errores[] = " El apellido es obligatorio ";
+        }
+
+        $queryVerificacion = " SELECT id_cliente FROM cliente WHERE email = $email ";
+        $verificacion = mysqli_query($link, $queryVerificacion);
+        var_dump($queryVerificacion);
+        if($verificacion){
+            $errores[] = " El email ya existe ";    
+        }
+
+
+        if(empty($errores)){
+            $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+            
+            //INSERTAR
+            $query = " INSERT INTO cliente (nombres, apellidos,telefono, email, contraseña) VALUES ('${nombre}', '${apellido}', '${telefono}', '${email}', '${passwordHash}')";
+            $resultado = mysqli_query($link, $query);
+
+            $queryID = " SELECT id_cliente FROM cliente WHERE email = '${email}' ";
+            $resultadoId = mysqli_query($link, $queryID);
+
+            $cliente = mysqli_fetch_assoc($resultadoId);
+            $clienteid = $cliente['id_cliente'];
+
+            $queryPedido = " INSERT INTO pedido (id_cliente) VALUES ($clienteid) ";
+            $insercionPedido = mysqli_query($link, $queryPedido);
+
+        }
+    } 
+
+    if($_SESSION){
+        header('Location: /');      
     }
-    
-    $insertQuery->close();
-    
-    $query->close();
-   
-    
 
-   
     
-    // Close DB connection
-    mysqli_close($db);
-   }
-
 ?>
 
 <!DOCTYPE html>
@@ -100,7 +117,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
                     <div class = "form-group">
                             <input type="submit" name = "submit"  value="Submit">
                     </div>
-                    <p>Tienes Cuenta <a href="login.php">Login
+                    <p>Tienes Cuenta <a href="Login.php">Login
                 </form>
             </div>
         </div>
