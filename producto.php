904146068482c0b5860f07b1e412627ef7e4aba5
HectorMCDLV <hectormcdlv@gmail.com>
2021-12-03 14:06:08 -0600

    unu

--- producto.php
+++ producto.php
@@ -1,38 +1,39 @@
-<?php 
-    require 'include/funciones.php';
-    incluirTemplate('header', 2);
+<?php
+    $id = $_GET['id'];
+    $request = $_GET['request'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if(!$id)
+    {
+        header('Location: /');
+    }
+
+    require __DIR__ . '/include/config/connect.php';
+    $db = conectarDB();
+
+    $query = " SELECT producto.id as id, producto.nombre as nombre, producto.imagen as imagen  FROM producto ";
+
+    $resultado = mysqli_query($db, $query);
+    $producto = mysqli_fetch_assoc($resultado);
 ?>  
 
-<body>
-    
+<main class="contenedor">
+    <?php if( intval( $request ) === 1 ): ?>
+        <p class="alerta exito">Producto Agregado al Carrito Exitosamente</p>
+    <?php endif; ?>
+    <h1><?php echo $producto['nombre']; ?></h1>
+    <div class="tienda">
+        <img class="tienda__imagen" src="../../portadas/<?php echo $comic['imagen']; ?>" alt="Producto">
+        <div class="tienda__info">
+            <p>
+                <?php echo $comic['sinopsis']; ?>
+            </p>
 
-    <main class="contenedor">
-        <h1>Biofert Plus </h1>
-        
-        <div class="producto__diseño">
-            <div class="producto" style="display:inline-block;vertical-align:top;">
-                    
-                    <img class="producto__imagen" src="img/biofert_plus.png" alt="imagen Biofert"  />
-                    
-            </div>
-        
-            <div class="producto__informacion" style="display:inline-block;vertical-align:top;padding: 3.5rem;" >
-                
-                <p class="Visualizacion__descripcion" > 
-                Biofert Plus. Es un producto natural a base de excreta de Bovino procesado en un reactor para lograr fermentación en cuatro dases,
-                al que se le agrega en cada fase componentes orgánicos que aumentan su efectividad al aplicarlo en el suelo creando un Universo 
-                microbiano benéfico al los cultivos. <br>
-                Biofer Plis Contiene: Fitohormonas que estimulan y vitalizan la germinación de semillas así como la formación del sistema radicular 
-                y tallos, floración, crecimientos, defensas y fruto.    
-                </p>
-
-                <input class="formulario__submit" type="submit" value="Agregar al Carrito">
-                
-            </div>
+            <form id="compra" class="formulario" method="POST">   
+                <input type="hidden" name="id_producto" value="<?php echo $comic['id'];?>">
+                <input class="formulario__campo" type="number" name="cantidad" placeholder="Cantidad" min="1" value="1">
+            </form>
+            <input class="boton w-sm-100" type="submit" form="compra" value="Agregar al Carrito">
         </div>
-    
-    </main>
-
-    <?php 
-        incluirTemplate('footer');
-    ?>
+    </div>
+</main>
